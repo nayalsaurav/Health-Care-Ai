1b2591fc8322bd33ceea03961574ac705e528ec4
Saurav Nayal <nayalsaurav@gmail.com>
2025-02-25 15:49:26 +0530

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const rootRouter = require("./routes/index");
 const app = express();
-
+app.use(express.json());
+app.use("/api/v1", rootRouter);
 app.get("/", (req, res) => {
   res.send("hi from backend");
 });

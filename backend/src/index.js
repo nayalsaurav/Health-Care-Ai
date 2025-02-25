26db31870df1b42817c1a0438528beae30bc3d3b
sassius <145041644+sassius@users.noreply.github.com>
2025-02-26 03:22:05 +0530

    added supabase authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const rootRouter = require("./routes/index.js");
+import  express  from "express";
+import rootRouter from "./routes/index.js"
 const app = express();
 app.use(express.json());
 app.use("/api/v1", rootRouter);
